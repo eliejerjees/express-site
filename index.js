d211d60bba13d4a5a0b372b7515e12a1f0e40e29
Elie Jerjees <eliejerjees7@gmail.com>
2026-02-08 14:46:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+// log the url
 let logger = (req, res, next) => {
     console.log(`url: ${req.url}`);
     return next();
 }
 
+// middleware added to each request
 app.use(logger);
+
+// check if user is logged in
+let auth = (req, res, next) => {
+    if(true)
+        return next();
+stack
+    // redirect to sign in page
+    res.redirect('/signin');
+}
+
+// add middleware to this request only
+app.get('./dashboard', auth, (req, res) => {
+    res.send('Hello Admin!')
+})
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 });
 
 app.get('/api', (req, res) => {
-    res.json({ "message:": 123} )
+    res.json({ "message:": "Hello, world!"} )
 });
 
 app.listen(PORT, () => {
